bf26e36df52ab78acc5c4f478e51c8bce2233c87
Rohit Senapati <rohitsenapati08@gmail.com>
2025-06-28 13:23:50 +0530

    Bug Fixed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -31,10 +31,11 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
   });
 }
+
 
 
 server.listen(PORT, () => {
