685226bf7c7e75784643c733bd0af2061b03a567
Rohit Senapati <rohitsenapati08@gmail.com>
2025-06-28 13:56:24 +0530

    bug fixed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -31,13 +31,10 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-  app.get("/:path*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
   });
-  
 }
-
-
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
